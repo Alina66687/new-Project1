5b06135cdd6203cb27bfefd467c925013a2cc016
alinaerhardt87@gmail.com <alinaerhardt87@gmail.com>
2021-09-29 12:05:37 +0200

    telefonbook

--- new Project/new Project.cpp
+++ new Project/new Project.cpp
@@ -1,27 +1,35 @@
 ﻿#include <iostream>
 #include <vector>
 #include <Windows.h>
+#include<fstream>
 using namespace std;
-
 struct Contact
 {
 	string name;
 	string number;
 };
 Contact create_contact();
+void autoload(vector<Contact>&);
+void autosave(vector<Contact>&);
 void output_book(vector<Contact>&);
-
+void edit_contact_in_book(vector<Contact>&, int);
+void output_contact_in_book(vector<Contact>&, int);
 void main()
 {
+	
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	int input;
 	vector<Contact> book;
+	autoload(book);
 	do
 	{
 		cout << "1 - Создать контакт " << endl
 			<< "2 - Просмотреть книгу  " << endl
+			<< "3 - Редактировать контакт" << endl
+			<< "4 - Очистить книгу" << endl
 			<< "0 - Выход " << endl;
+		
 		cin >> input;
 		switch (input)
 		{
@@ -30,11 +38,30 @@ void main()
 			break;
 		case 2:
 			output_book(book);
+			system("pause");
+			break;
+		case 3:
+			cout << "Введите № контакта: " << endl;
+			int num;
+			cin >> num;
+			if (num > 0 && num <= book.size())
+			{
+				edit_contact_in_book(book, num - 1);
+			}
+			else
+			{
+				cerr << "Error " << endl;
+			}
+			break;
+		case 4:
+			book.clear();
 			break;
 		default:
 			break;
 		}
+		autosave(book);
 	} while (input != 0);
+
 }
 
 
@@ -52,6 +79,57 @@ void output_book(vector<Contact>& book)
 {
 	for (int i = 0; i < book.size(); i++)
 	{
-		cout << "Имя: " << book[i].name << " Номер: " << book[i].number << endl;
+		output_contact_in_book(book, i);
+	}
+}
+
+void edit_contact_in_book(vector<Contact>& book, int index) //редактировать
+{
+	output_contact_in_book(book, index);
+	int a;
+	cout << "Если вы хотите поменять имя, введите 1 " << endl
+		<< "Если вы хотите поменять номер, введите 2 " << endl
+		<< "Eсли вы передумали, введите любое другое число " << endl;
+	cin >> a;
+	switch (a)
+	{
+	case 1:
+		cout << "Введите новое имя: ";
+		cin >> book[index].name;
+		break;
+	case 2:
+		cout << "Ввeдите новый номер: ";
+		cin >> book[index].number;
+	default:
+		break;
+	}
+}
+void output_contact_in_book(vector<Contact>& book, int index)
+{
+	cout << "№ " << index + 1 << " Имя: " << book[index].name << " Номер: " << book[index].number << endl;
+}
+void autosave(vector<Contact>& book)
+{
+	ofstream tbook;
+	tbook.open("book.txt");
+	for (int a = 0; a < book.size(); a++)
+	{
+		tbook << book[a].name << endl << book[a].number;
+		if (a < book.size() - 1)
+		{
+			tbook << endl;
+		}
+	}
+	tbook.close();
+}
+void autoload(vector<Contact>& book)
+{
+	ifstream ibook;
+	ibook.open("book.txt");
+	while (!ibook.eof())
+	{
+		Contact contact;
+		ibook >> contact.name >> contact.number;
+		book.push_back(contact);
 	}
 }
